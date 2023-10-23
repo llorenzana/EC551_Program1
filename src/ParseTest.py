7ee1930e346ff4636b97c4edbaea08b4b8da1d47
llorenzana <91199802+llorenzana@users.noreply.github.com>
2023-10-23 11:32:02 -0400

    updated function
    got EPI and PI to work

--- src/ParseTest.py
+++ src/ParseTest.py
@@ -1,13 +1,11 @@
 from itertools import product
-from sympy.logic.boolalg import Or, And, Not
-from sympy import symbols, parse_expr, to_cnf, simplify_logic
 
 class BooleanFunction:
     def __init__(self, expression, variables):
         self.expression = expression
         self.variables = variables
 
-    def generateTerms(self):
+    def generate_terms(self):
         minterms = []
         maxterms = []
 
@@ -30,7 +28,7 @@ class BooleanFunction:
         expanded_sum_of_minterms = self.expand_minterms(minterms)
         expanded_product_of_maxterms = self.expand_maxterms(maxterms)
 
-        return minterms, expanded_sum_of_minterms , maxterms, expanded_product_of_maxterms
+        return minterms, expanded_sum_of_minterms, maxterms, expanded_product_of_maxterms
 
     def expand_minterms(self, minterms):
         expanded_sum_of_minterms = []
@@ -65,45 +63,16 @@ class BooleanFunction:
             expanded_product_of_maxterms.append(term)
 
         return " & ".join(expanded_product_of_maxterms)
-    
-    def find_prime_implicants(self):
-        simplified_function = simplify_logic(self.expression)
 
-        prime_implicants = simplified_function.args if isinstance(simplified_function, Or) else [simplified_function]
-
-        return len(prime_implicants)
-
-    def find_essential_prime_implicants(self, minterms):
-        prime_implicants = self.find_prime_implicants()
-        essential_prime_implicants = []
-
-        for minterm in minterms:
-            count = 0
-            essential_implicant = None
-            for prime_implicant in prime_implicants:
-                if minterm in prime_implicant.args:
-                    count += 1
-                    essential_implicant = prime_implicant
-
-            if count == 1:
-                essential_prime_implicants.append(essential_implicant)
-
-        return essential_prime_implicants   
-         
-#  Example usage:
+# Example usage:
 print("Welcome: Please select which you would like to do 1 or 2")
 expression = input("Boolean Equation, please use ~ to indicate a NOT: ")
 v_in = input("Enter input variables separated by a space: ")
 vars = v_in.split()
 
 boolean_function = BooleanFunction(expression, vars)
-minterms, CSOP, maxterms, CPOS = boolean_function.generateTerms()
+minterms, _, maxterms, _ = boolean_function.generate_terms()
 
 print("Generated Minterms:", minterms)
 print("Generated Maxterms:", maxterms)
-
-
-
-
-
 
